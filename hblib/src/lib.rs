898ca14160d732092800d8b9695cfb80525441b0
Kunal Mehta <legoktm@member.fsf.org>
2021-02-20 19:05:47 -0800

    Fix log rotation

--- hblib/src/lib.rs
+++ hblib/src/lib.rs
@@ -51,16 +51,15 @@ pub fn print_diff(old: &str, new: &str) -> bool {
 }
 
 pub fn setup_logging(name: &str) -> LoggerHandle {
-    use flexi_logger::{opt_format, Age, Cleanup, Criterion, Duplicate, Logger, Naming};
+    use flexi_logger::{opt_format, Cleanup, Criterion, Duplicate, Logger, Naming};
     Logger::with_str(format!("info, {}=debug", name))
         .log_to_file()
         .duplicate_to_stdout(Duplicate::Info)
         .format(opt_format)
-        .suppress_timestamp()
         .append()
         .use_buffering(true)
         .rotate(
-            Criterion::Age(Age::Day),
+            Criterion::Size(5_000_000),
             Naming::Timestamps,
             Cleanup::KeepLogFiles(30),
         )
